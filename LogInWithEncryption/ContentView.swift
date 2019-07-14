42d994c03d0d606923b5a752340a1ba8fc321df9
Manenga Mungandi <manengam@gmail.com>
2019-07-14 21:27:13 +0200

    Initial Commit

---
+++ LogInWithEncryption/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LogInWithEncryption
+//
+//  Created by Manenga Mungandi on 2019/07/14.
+//  Copyright © 2019 Manenga Mungandi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
